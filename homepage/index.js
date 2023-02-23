cf595593b58f84eefdc2e32919ec61c0aafcaed8
thesujalogy <thesujalogy@gmail.com>
2023-02-23 16:43:31 +0530

    conflict resolved

---
+++ homepage/index.js
@@ -0,0 +1,19 @@
+// making dropdowns of the headers
+//---------------> this is second third nav div h4 tags---->
+let head1 = document.getElementById("headone");
+let head2 = document.getElementById("headtwo");
+let head3 = document.getElementById("headthree");
+let head4 = document.getElementById("headfour");
+let head5 = document.getElementById("headfive");
+let head6 = document.getElementById("headsix");
+let head7 = document.getElementById("headseven");
+let head8 = document.getElementById("headeight");
+
+head1.addEventListener("mouseover",()=> {
+    
+})
+head1.addEventListener("mouseout",()=> {
+    head1.style.color = "#000042";
+})
+
+
