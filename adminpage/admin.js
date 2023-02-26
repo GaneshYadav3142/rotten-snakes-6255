4369331c7a7b3c134187f9cb532c04aeb029cbfd
unknown <ganeshyadavstudies@gmail.com>
2023-02-26 19:18:03 +0530

    added

--- adminpage/admin.js
+++ adminpage/admin.js
@@ -1,19 +1,32 @@
 
     // 1. catch the form
+    
+
+    let productDB=JSON.parse(localStorage.getItem("AllProduct"))||[]
     let form = document.querySelector('form');
     form.addEventListener('submit',function(event){
         event.preventDefault();
         let formData = {
-            name: form.name.value,
-            desc: form.desc.value,
-            price: form.price.value,
-            url: form.url.value
+            SpecColor: form.SpecColor.value,
+            SpecSize: form.SpecSize.value,
+            ProductCategory: form.ProductCategory.value,
+            FrameShape: form.FrameShape.value,
+            ProductRating: form.ProductRating.value,
+            ProductTitle: form.ProductTitle.value,
+            qty:1,
+            id: form.id.value,
+            ProductPrice: form.ProductPrice.value,
+            BackgroundImage: form.BackgroundImage.value,
+            FrontImage: form.FrontImage.value,
+            imageModel1: form.imageModel1.value,
+            imageModel2: form.imageModel2.value
         }
         //5. we push this obj into our array
         productDB.push(formData);
         // 6. we store the data in local storage.
         // we are trying to store array and object so we need to use stringify
-        localStorage.setItem('productDB',JSON.stringify(productDB));
+        localStorage.setItem('AllProduct',JSON.stringify(productDB));
 
         alert('product added to DB!');
+        window.location.href="../Product_Page/AllInput/ProductPage.html"
     })
